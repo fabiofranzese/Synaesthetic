16d6eabb16d2ca008400d84ac35a1097dbf00b18
Fabio Franzese <fabiofranzeseuni@gmail.com>
2024-10-30 10:57:54 +0100

    Initial Commit

---
+++ actantes/actantesApp.swift
@@ -0,0 +1,17 @@
+//
+//  actantesApp.swift
+//  actantes
+//
+//  Created by Fabio on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct actantesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
