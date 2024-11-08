7e0c4032842cbf90d70af2ef01510935ce52ab5d
GabborZ <gabrielefiore99@gmail.com>
2024-11-08 11:30:15 +0100

    Add files via upload

---
+++ wave/wave/waveApp.swift
@@ -0,0 +1,17 @@
+//
+//  waveApp.swift
+//  wave
+//
+//  Created by Gabriele Fiore on 08/11/24.
+//
+
+import SwiftUI
+
+@main
+struct waveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
