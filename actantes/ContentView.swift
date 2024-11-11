03f80b913f936329c13fd625ad4cdc8543932226
alessiafrezzetti <cuoringiro@gmail.com>
2024-11-11 12:10:00 +0100

    Update ContentView.swift

--- actantes/ContentView.swift
+++ actantes/ContentView.swift
@@ -1,18 +1,37 @@
 //
-//  ContentVie.swift
-//  actantes
+//  ContentView.swift
 //
-//  Created by alessia frezzetti on 11/11/24.
+//  Created by Gabriele Fiore on 08/11/24.
 //
 
 import SwiftUI
 
-struct ContentVie: View {
+struct ContentView: View {
+    // Capture the start time when the view is created
+    let start = Date()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TimelineView(.animation) { tl in
+            // Calculate elapsed time since the start
+            let time = start.distance(to: tl.date)
+            
+            // Apply the sinebow shader effect to a fullscreen Rectangle
+            Rectangle()
+                .visualEffect { content, proxy in
+                    content.colorEffect(
+                        ShaderLibrary.sinebow( // Reference to the Metal shader
+                            .float2(proxy.size), // Pass the size of the view
+                            .float(time)         // Pass the elapsed time
+                        )
+                    )
+                }
+                .ignoresSafeArea() // Make the Rectangle fill the entire screen
+        }
     }
 }
 
-#Preview {
-    ContentVie()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
